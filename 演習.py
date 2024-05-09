da9fed8a78de4e02fcca8285d45bfe4ce6936786
Miyazaki <e225765@ie.u-ryukyu.ac.jp>
2024-05-09 16:41:59 +0900

    ensyu1.2ok?

--- 演習.py
+++ 演習.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 #%matplotlib inline
 
@@ -8,29 +9,32 @@ def true_function(x):
     y=math.sin(math.pi * x * 0.8) * 10
     return y
 
-elements_size=20
+elements_size=100
 
 Yarray=np.ndarray(elements_size)
 #xarray=np.random.rand(elements_size)
 #xarray=np.zeros(elements_size)
-#xarray=np.linspace(-1,1,elements_size)
-xarray=np.random.uniform(-1.0,1.0,20)
-print(xarray)
+xarray=np.linspace(-1,1,elements_size)
+xarray2=np.random.uniform(-1.0,1.0,20)
+
 k=0
 for i in xarray:
     y=true_function(i)
     Yarray[k]=y#20この真値
     k=k+1
-print(xarray[0])
 
-array1_2=np.column_stack([xarray,Yarray])
-print(np.shape(array1_2))
-print(array1_2)
+k=0
+Yarray2=np.ndarray(20)
 
+for i in xarray2:
+    y=true_function(i)
+    Yarray2[k]=y#20この真値
+    k=k+1
 
+array1_2=np.column_stack([xarray2,Yarray2])
 
-
-#print(Yarray)
+df=pd.DataFrame(array1_2,columns=["観測点","真値点"])
+df.plot.scatter(x="観測点",y="真値点")
 
 plt.plot(xarray,Yarray,label="sin")
 plt.legend()
