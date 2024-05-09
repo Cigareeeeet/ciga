fb05773fc160224a9d5f5f4f3fa0c22d0e0f10ac
Miyazaki <e225765@ie.u-ryukyu.ac.jp>
2024-05-09 14:59:57 +0900

    test

--- 演習.py
+++ 演習.py
@@ -8,22 +8,32 @@ def true_function(x):
     y=math.sin(math.pi * x * 0.8) * 10
     return y
 
-elements_size=1000
+elements_size=20
 
 Yarray=np.ndarray(elements_size)
 #xarray=np.random.rand(elements_size)
 #xarray=np.zeros(elements_size)
-xarray=np.linspace(-1,1,elements_size)
+#xarray=np.linspace(-1,1,elements_size)
+xarray=np.random.uniform(-1.0,1.0,20)
+print(xarray)
 k=0
 for i in xarray:
     y=true_function(i)
-    Yarray[k]=y
+    Yarray[k]=y#20この真値
     k=k+1
+print(xarray[0])
+
+array1_2=np.column_stack([xarray,Yarray])
+print(np.shape(array1_2))
+print(array1_2)
+
+
+
 
 #print(Yarray)
 
 plt.plot(xarray,Yarray,label="sin")
 plt.legend()
-plt.savefig("ex1-1.png")
+plt.savefig("ex1-2.png")
 plt.show()
 
