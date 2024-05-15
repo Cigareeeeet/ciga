5e880c399c444883248d545f95845f3dad68051c
Miyazaki <e225765@ie.u-ryukyu.ac.jp>
2024-05-16 02:16:35 +0900

    ensyu1.3ok?

--- 演習.py
+++ 演習.py
@@ -30,14 +30,20 @@ for i in xarray2:
     y=true_function(i)
     Yarray2[k]=y#20この真値
     k=k+1
+#Yarray2にノイズをかける
+noize=(np.random.normal(loc=0.0, scale=2.0, size=20))/2
+Yarray3=Yarray2+noize
 
-array1_2=np.column_stack([xarray2,Yarray2])
 
-df=pd.DataFrame(array1_2,columns=["観測点","真値点"])
-df.plot.scatter(x="観測点",y="真値点")
+array1_2=np.column_stack([xarray2,Yarray2,Yarray3])
+print(array1_2)
+df=pd.DataFrame(array1_2,columns=["観測点","真値点","観測値"])
+print(df)
+ax=df.plot.scatter(x="観測点",y="真値点",label="true value",c="g")
+df.plot.scatter(x="観測点",y="観測値",c="r",ax=ax, label="observed value")
 
 plt.plot(xarray,Yarray,label="sin")
 plt.legend()
-plt.savefig("ex1-2.png")
+plt.savefig("ex1-3.png")
 plt.show()
 
