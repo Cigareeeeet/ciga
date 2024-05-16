63ed8324193e9a2bcbf29dd191590296d640dcfc
Miyazaki <e225765@ie.u-ryukyu.ac.jp>
2024-05-16 13:53:12 +0900

    test3

---
+++ regression.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class LinearRegression:
+    x=None
+    theta=None
+    y=None
+
+    def fit(self,x,y): #モデルに学習を適用する関数
+        temp=np.linalg.inv(np.dot(x.T,x))
+        self.theta=np.dot(np.dot(temp,x.T),y)
+
+    def predict(self, X): #予測する関数
+        return np.dot(x, self.theta)
+
+    def score(self,x,y):
+        pass
+
+
+        
